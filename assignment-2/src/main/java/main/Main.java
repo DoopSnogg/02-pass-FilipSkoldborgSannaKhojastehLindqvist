1fe16f5da526018ff34b30e24f2cc72107daed09
DoopSnogg <50363906+DoopSnogg@users.noreply.github.com>
2021-03-30 14:08:41 +0200

    Update Main.java

--- assignment-2/src/main/java/main/Main.java
+++ assignment-2/src/main/java/main/Main.java
@@ -7,10 +7,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		BasicCalculator baseCalc = new BasicCalculator();
-		System.out.println(baseCalc.addition(8, 8));
+		System.out.println(baseCalc.addition(1, 2));
+		System.out.println(baseCalc.subtraction(1, 2));
+		System.out.println(baseCalc.multiplication(1, 2));
+		System.out.println(baseCalc.division(1, 2));
 
 		AdvancedCalculator advCalc = new AdvancedCalculator();
-		System.out.println(advCalc.max(15, 14));
+		System.out.println(advCalc.addition(1, 2));
+		System.out.println(advCalc.subtraction(1, 2));
+		System.out.println(advCalc.multiplication(1, 2));
+		System.out.println(advCalc.division(1, 2));
+		System.out.println(advCalc.raisedToThePowerOf(1, 2));
+		System.out.println(advCalc.max(1, 2));
+		System.out.println(advCalc.min(1, 2));
+		System.out.println(advCalc.absoluteValue(1));
+		System.out.println(advCalc.squareRoot(1));
 
 	}
 }
